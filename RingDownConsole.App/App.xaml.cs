20323d675f11ab80bccb2327d49fc722b9c62665
Edward Miller <edward.miller@catconsult.com>
2017-12-29 16:45:29 -0600

    Add native app

---
+++ RingDownConsole.App/App.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System.Windows;
+using Serilog;
+using Serilog.Events;
+
+namespace RingDownConsole.App
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            Log.Logger = new LoggerConfiguration()
+                .MinimumLevel.Debug()
+                .MinimumLevel.Override("Microsoft", LogEventLevel.Information)
+                .Enrich.FromLogContext()
+                .WriteTo.Console()
+                .CreateLogger();
+        }
+    }
+}
