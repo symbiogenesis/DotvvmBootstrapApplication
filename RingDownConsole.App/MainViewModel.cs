93f13d8a7e7ed32c59bdd0ab2bacdc8ba3926c30
Edward Miller <edward.miller@catconsult.com>
2018-01-04 13:21:33 -0600

    Convert to MVVM and add vector images

---
+++ RingDownConsole.App/MainViewModel.cs
@@ -0,0 +1,349 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+using Dataq.Devices;
+using Dataq.Devices.DI1100;
+using Dataq.Misc;
+using Dataq.Protocols.Enums;
+using Serilog;
+
+namespace RingDownConsole.App
+{
+    public class MainViewModel : INotifyPropertyChanged
+    {
+        private const int SAMPLE_RATE = 39000;
+
+        private bool _showSettings;
+        private bool _showNameEntry;
+        private bool _showDeviceNotFound;
+        private ICommand findDeviceCommand;
+        private Device _targetDevice;
+        private Task _taskRead;
+        private CancellationTokenSource _cancelRead;
+        private IProgress<string> _progress;
+        private List<string> _output;
+
+        public MainViewModel()
+        {
+            _output = new List<string>();
+            _progress = new Progress<string>(_output.Add);
+
+            FindDeviceCommand = new RelayCommand(SearchDevices);
+
+            PopulateColors();
+
+            new Action(async () =>
+            {
+                var deviceFound = await FindDevice();
+
+                if (deviceFound)
+                    await ToggleDataAcquisition();
+            }).Invoke();
+        }
+
+        public bool ShowSettings
+        {
+            get { return _showSettings; }
+            set
+            {
+                if (_showSettings != value)
+                {
+                    _showSettings = value;
+                    RaisePropertyChanged();
+                }
+            }
+        }
+
+        public bool ShowDeviceNotFound
+        {
+            get { return _showDeviceNotFound; }
+            set
+            {
+                if (_showDeviceNotFound != value)
+                {
+                    _showDeviceNotFound = value;
+                    RaisePropertyChanged();
+                }
+            }
+        }
+
+        public bool ShowNameEntry
+        {
+            get { return _showNameEntry; }
+            set
+            {
+                if (_showNameEntry != value)
+                {
+                    _showNameEntry = value;
+                    RaisePropertyChanged();
+                }
+            }
+        }
+
+        public ICommand FindDeviceCommand
+        {
+            get
+            {
+                return findDeviceCommand;
+            }
+            set
+            {
+                findDeviceCommand = value;
+            }
+        }
+
+
+        private void PopulateColors()
+        {
+            var colors = new List<string>();
+
+            foreach (var color in Enum.GetValues(typeof(LedColor1)))
+            {
+                colors.Add(color.ToString());
+            }
+        }
+
+        private async void SearchDevices(object obj)
+        {
+            await FindDevice();
+        }
+
+        private async Task<bool> FindDevice()
+        {
+            //  Get a list of devices with model DI-1100
+            IReadOnlyList<IDevice> AllDevices = await Discovery.ByModelAsync(typeof(Device));
+
+            var anyDevice = AllDevices.Count > 0;
+
+            if (anyDevice)
+            {
+                ShowDeviceNotFound = false;
+
+                Log.Information("Found a DI-1100.");
+                //  Cast first device from generic device to specific DI-1100 type
+                _targetDevice = ((Device) (AllDevices[0]));
+                await _targetDevice.ConnectAsync();
+                //  Ensure it's stopped
+                await _targetDevice.AcquisitionStopAsync();
+                //  Query device for some info
+                await _targetDevice.QueryDeviceAsync();
+                //  Print queried info to GUI
+                Log.Information($"Manufacturer: {_targetDevice.Manufacturer}");
+                Log.Information($"Model: {_targetDevice.Model}");
+                Log.Information($"Serial number: {_targetDevice.Serial}");
+                Log.Information($"Firmware revision: {_targetDevice.FirmwareRevision.ToString()}");
+            }
+            else
+            {
+                ShowDeviceNotFound = true;
+                Log.Information("No DI-1100 found.");
+            }
+
+            return anyDevice;
+        }
+
+        private async Task GetChannelData()
+        {
+            // capture the first channel programmed as an input (MasterChannel)
+            // and use it to track data availability for all input channels
+            IChannelIn MasterChannel = null;
+            for (int index = 0; (index <= _targetDevice.Channels.Count); index++)
+            {
+                if (_targetDevice.Channels[index].GetType().GetInterfaces().Contains(typeof(IChannelIn)))
+                {
+                    MasterChannel = (IChannelIn) _targetDevice.Channels[index];
+                    //  we have our channel 
+                    break;
+                }
+            }
+
+            //  Keep reading while acquiring data
+            while (_targetDevice.IsAcquiring)
+            {
+                //  Read data and catch if cancelled (to exit loop and continue)
+                try
+                {
+                    // throws an error if acquisition has been cancelled
+                    // otherwise refreshes the buffer DataIn with new data
+                    await _targetDevice.ReadDataAsync(_cancelRead.Token);
+                }
+                catch (OperationCanceledException ex)
+                {
+                    Log.Information("Acquisition cancelled");
+                    break;
+                }
+
+                // get here if acquisition is still active
+                if (MasterChannel.DataIn.Count == 0)
+                {
+                    // get here if no data in the channel buffer
+                    // TODO: Continue While... Warning!!! not translated
+                }
+
+                //  We have data. Convert it to strings
+                string temp = "";
+                string temp1 = "";
+                for (int index = 0; (index <= (MasterChannel.DataIn.Count - 1)); index++)
+                {
+                    //  this is the row (scan) counter
+                    foreach (var ch in _targetDevice.Channels)
+                    {
+                        // this is the column (channel) counter
+                        // get a channel value and convert it to a string
+                        if (ch.GetType().GetInterfaces().Contains(typeof(IChannelIn)))
+                        {
+                            temp1 = ((IChannelIn) (ch)).DataIn[index].ToString();
+                            // trim the lstOutput to prevent overrunning the display
+                            if (temp1.Length > 7)
+                            {
+                                temp1 = (temp1.Substring(0, 7) + ", ");
+                            }
+                            else
+                            {
+                                temp1 = (temp1 + ", ");
+                            }
+
+                            temp = (temp + temp1);
+                            // append the channel value to the lstOutput
+                        }
+                    }
+
+                    _progress.Report(temp);
+                }
+
+                // get the next row
+                //  purge displayed data
+                foreach (var ch in _targetDevice.Channels)
+                {
+                    if (ch.GetType().GetInterfaces().Contains(typeof(IChannelIn)))
+                    {
+                        ((IChannelIn) (ch)).DataIn.Clear();
+                    }
+                }
+            }
+
+            _progress.Report($"Stopped{Environment.NewLine}");
+        }
+
+        private void ConfigureAnalogChannels()
+        {
+            AnalogVoltageIn AnalogChan = ((AnalogVoltageIn) (_targetDevice.ChannelFactory(typeof(AnalogVoltageIn), 1)));
+        }
+
+        private bool SampleRateBad()
+        {
+            // ensure that the sample rate per channel is not out of range
+            Dataq.Collections.IReadOnlyRange<double> SampleRateRange = ((Dataq.Devices.Dynamic.IReadOnlyChannelSampleRate) (_targetDevice.Channels[0])).GetSupportedSampleRateRange(_targetDevice);
+            if (!SampleRateRange.ContainsValue(SAMPLE_RATE))
+            {
+                MessageBox.Show($"Selected sample rate is outside the range of  {SampleRateRange.Minimum.ToString()} to {SampleRateRange.Maximum.ToString()} Hz inclusive for your current channel settings.", "Sample Rate Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                return true;
+            }
+
+            return false;
+        }
+
+        private void Form1_FormClosing(object sender, EventArgs e)
+        {
+            if (_targetDevice != null)
+            {
+                _targetDevice.Dispose();
+                _targetDevice = null;
+            }
+        }
+
+        private async void SetLightColor(LedColor1 color)
+        {
+            await _targetDevice.Protocol.SetLedColorAsync(color);
+        }
+
+        private async Task ToggleDataAcquisition()
+        {
+            if (_cancelRead != null)
+            {
+                // get here if an acquisition process is in progress and we've been commanded to stop
+                _cancelRead.Cancel();
+                // cancel the read process
+                _cancelRead = null;
+                await _taskRead;
+                // wait for the read process to complete
+                _taskRead = null;
+                await _targetDevice.AcquisitionStopAsync();
+                // stop the device from acquiring
+            }
+            else
+            {
+                // get here if we're starting a new acquisition process
+                _targetDevice.Channels.Clear();
+                // initialize the device
+                this.ConfigureAnalogChannels();
+                if (this.SampleRateBad())
+                {
+                    // get here if requested sample rate is out of range
+                    // It's a bust, so...
+                    return;
+                }
+
+                // otherwise, the selected sample rate is good, so use it
+                _targetDevice.SetSampleRateOnChannels(SAMPLE_RATE);
+                try
+                {
+                    await _targetDevice.InitializeAsync();
+                    // configure the device as defined. Errors if no channels are enabled
+                }
+                catch (Exception ex)
+                {
+                    // Detect if no channels are enabled, and bail if so. 
+                    MessageBox.Show("Please enable at least one analog channel or digital port.", "No Enabled Channels", MessageBoxButton.OK, MessageBoxImage.Error);
+                    return;
+                }
+
+                // now determine what sample rate per channel the device is using from the 
+                // first enabled input channel, and display it
+                ChannelIn FirstInChannel;
+                bool NoInputChannels = true;
+                for (int index = 0; (index <= (_targetDevice.Channels.Count - 1)); index++)
+                {
+                    if (_targetDevice.Channels[index].GetType().GetInterfaces().Contains(typeof(IChannelIn)))
+                    {
+                        FirstInChannel = (ChannelIn) _targetDevice.Channels[index];
+                        NoInputChannels = false;
+                        break;
+                    }
+                }
+
+                if (NoInputChannels)
+                {
+                    MessageBox.Show("Please configure at least one analog channel or digital port as an input", "No Inputs Enabled", MessageBoxButton.OK, MessageBoxImage.Error);
+                    return;
+                }
+
+                // Everything is good, so...
+                _cancelRead = new CancellationTokenSource();
+                //  Create the cancellation token
+                await _targetDevice.AcquisitionStartAsync();
+                // start acquiring
+                //  NOTE: assumes at least one input channel enabled
+                //  Start a task in the background to read data
+                _taskRead = Task.Run(async () => await GetChannelData(), _cancelRead.Token);
+            }
+        }
+
+        #region INotifyPropertyChanged Members
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void RaisePropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        #endregion
+    }
+}
