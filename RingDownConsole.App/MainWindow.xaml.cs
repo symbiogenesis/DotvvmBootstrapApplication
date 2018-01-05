45a1b99777bda1b81b9528a4a873335d8228f73b
Edward Miller <edward.miller@catconsult.com>
2018-01-05 09:29:17 -0600

    remove close button

--- RingDownConsole.App/MainWindow.xaml.cs
+++ RingDownConsole.App/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Windows;
+﻿using System;
+using System.Windows;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 
@@ -9,9 +10,25 @@ namespace RingDownConsole.App
     /// </summary>
     public partial class MainWindow : Window
     {
+        // Prep stuff needed to remove close button on window
+        private const int GWL_STYLE = -16;
+        private const int WS_SYSMENU = 0x80000;
+        [System.Runtime.InteropServices.DllImport("user32.dll", SetLastError = true)]
+        private static extern int GetWindowLong(IntPtr hWnd, int nIndex);
+        [System.Runtime.InteropServices.DllImport("user32.dll")]
+        private static extern int SetWindowLong(IntPtr hWnd, int nIndex, int dwNewLong);
+
+        private void ToolWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            // Code to remove close box from window
+            var hwnd = new System.Windows.Interop.WindowInteropHelper(this).Handle;
+            SetWindowLong(hwnd, GWL_STYLE, GetWindowLong(hwnd, GWL_STYLE) & ~WS_SYSMENU);
+        }
+
         public MainWindow()
         {
             InitializeComponent();
+            Loaded += ToolWindow_Loaded;
             Style = (Style) FindResource(typeof(Window));
             //Icon = ConvertToBitmapSource((Path)this.Resources["CommunicationErrorIcon"]);
         }
