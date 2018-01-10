5ac78cf56f78ef74e026270fbbd004d0d522dbdf
Edward Miller <edward.miller@catconsult.com>
2018-01-10 16:34:52 -0600

    add serial number to location table

--- RingDownConsole.Models/Location.cs
+++ RingDownConsole.Models/Location.cs
@@ -10,6 +10,8 @@ namespace RingDownConsole.Models
     {
         public int Id { get; set; }
 
+        public string SerialNumber { get; set; }
+
         public string Code { get; set; }
 
         public string Name { get; set; }
