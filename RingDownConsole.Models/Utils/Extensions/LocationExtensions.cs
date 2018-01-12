35e76cac93c6f00d1f6f5a6e0329beca2873dc8c
Edward Miller <edward.miller@catconsult.com>
2018-01-12 13:26:20 -0600

    don't use packages.config

--- RingDownConsole.Models/Utils/Extensions/LocationExtensions.cs
+++ RingDownConsole.Models/Utils/Extensions/LocationExtensions.cs
@@ -1,10 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Net.Http;
-using System.Text;
+﻿using System.Net.Http;
 using System.Threading.Tasks;
 using Newtonsoft.Json;
-using RingDownConsole.Interfaces;
 
 namespace RingDownConsole.Utils.Extensions
 {
