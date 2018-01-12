bb8a07bc70a8239ce27a557f4527eb4155c969d7
Edward Miller <edward.miller@catconsult.com>
2018-01-11 18:09:12 -0600

    add new Location if Locations table is null

--- RingDownConsole.WebAPI/Controllers/LocationController.cs
+++ RingDownConsole.WebAPI/Controllers/LocationController.cs
@@ -14,27 +14,41 @@ namespace RingDownConsole.WebAPI.Controllers
         [HttpGet("getlocation/{id}")]
         public async Task<Location> GetBySerialNumber(string serialNumber)
         {
+            if (!_context.Locations.Any())
+            {
+                return await AddLocation(serialNumber);
+            }
+
             var location = _context.Locations.FirstOrDefault(l => l.SerialNumber == serialNumber);
 
             if (location == default(Location))
             {
-                var maxId = _context.Locations.Max(l => l.Id);
-
-                maxId++;
-
-                var newLocation = new Location
-                {
-                    Id = maxId,
-                    SerialNumber = serialNumber,
-                    Code = maxId.ToString(),
-                    Name = maxId.ToString()
-                };
-
-                await _context.Locations.AddAsync(newLocation);
-                await _context.SaveChangesAsync();
+                return await AddLocation(serialNumber);
             }
+            else
+            {
+                return location;
+            }
+        }
 
-            return location;
+        private async Task<Location> AddLocation(string serialNumber)
+        {
+            var maxId = _context.Locations.Max(l => l.Id);
+
+            maxId++;
+
+            var newLocation = new Location
+            {
+                Id = maxId,
+                SerialNumber = serialNumber,
+                Code = maxId.ToString(),
+                Name = maxId.ToString()
+            };
+
+            await _context.Locations.AddAsync(newLocation);
+            await _context.SaveChangesAsync();
+
+            return newLocation;
         }
     }
 }
