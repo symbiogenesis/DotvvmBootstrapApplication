cc33d618ed30eb7e6df3b0540dcd46a43410e86b
Reginald Dean <reginald.dean@cityofchicago.org>
2018-01-16 10:12:41 -0600

    Changed name of .aspx pages to reflect database naming convention

---
+++ RingDownCentralConsole/Administration/Locations.aspx
@@ -0,0 +1,120 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Locations.aspx.cs" Inherits="RingDownCentralConsole.Locations" %>
+
+  
+<asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
+    <style>
+        .center-div
+        {
+          text-align: center;
+          font-size: large;
+          font-weight: bold;
+        }
+        .auto-style1 {
+            height: 24px;
+        }
+        th
+    {
+        text-align:center;
+    }
+    
+    </style>
+      
+
+    <div  class="center-div">
+
+      Stations
+
+    </div>
+
+
+    <div>
+
+        <p></p>
+
+    </div>
+       <asp:UpdatePanel ID="UpdatePanel1" runat="server">
+        <ContentTemplate>
+
+    <asp:Label id="Msg" runat="server" ForeColor="maroon" /><br />
+        <div id = "dvGrid" style ="padding:10px;width:550px">
+
+
+  
+<asp:GridView ID="GridView1" runat="server"  Width = "550px" AutoGenerateColumns = "false" Font-Names = "Arial" Font-Size = "11pt" 
+AlternatingRowStyle-BackColor = "white" HeaderStyle-BackColor = "#507CD1" AllowPaging ="true"  ShowFooter = "true" 
+OnPageIndexChanging = "OnPaging" onrowediting="EditLocation" onrowupdating="UpdateLocation"  onrowcancelingedit="CancelEdit"
+PageSize = "100" EmptyDataText="No Station Records Entered" >
+    
+<Columns>
+    <asp:TemplateField ItemStyle-Width = "50px" visible="false"  >
+    <ItemTemplate>
+        <asp:Label ID="lblLocationId" runat="server" Text='<%# Eval("Id")%>'></asp:Label>
+    </ItemTemplate>    
+</asp:TemplateField>
+
+     
+
+    <asp:TemplateField ItemStyle-Width = "400px"  HeaderText = "Location ID">
+    <ItemTemplate>
+        <asp:Label ID="lblLocationCode" runat="server" Text='<%# Eval("Code")%>'></asp:Label>
+    </ItemTemplate>
+    <EditItemTemplate>
+        <asp:TextBox ID="txtLocationCode" runat="server" Text='<%# Eval("Code")%>'></asp:TextBox>
+         <asp:RequiredFieldValidator ID="ReqLocationCode" runat="server" ControlToValidate="txtLocationCode" ErrorMessage="*" ForeColor="Red">
+         </asp:RequiredFieldValidator>
+    </EditItemTemplate> 
+    <FooterTemplate>
+        <asp:TextBox ID="txtLocationCode" Width = "200px" MaxLength = "60" runat="server"></asp:TextBox>     
+         <asp:RequiredFieldValidator ID="ReqLocationCode" runat="server" ControlToValidate="txtLocationCode" ErrorMessage="*" ForeColor="Red" ValidationGroup="GroupFooterInsert">
+         </asp:RequiredFieldValidator>
+    </FooterTemplate>
+</asp:TemplateField>
+    
+<asp:TemplateField ItemStyle-Width = "400px"  HeaderText = "Location Name"  >
+     <ItemTemplate>
+         <asp:Label ID="lblLocationName" runat="server" Text='<%# Eval("Name")%>'></asp:Label>
+     </ItemTemplate>
+    <EditItemTemplate>
+         <asp:TextBox ID="txtLocationName" runat="server" Text='<%# Eval("Name")%>'></asp:TextBox>
+           <asp:RequiredFieldValidator ID="ReqLocationName" runat="server" ControlToValidate="txtLocationName" ErrorMessage="*" ForeColor="Red">
+         </asp:RequiredFieldValidator>
+    </EditItemTemplate> 
+    <FooterTemplate>
+       <asp:TextBox ID="txtLocationName" Width = "200px" MaxLength = "60" runat="server"></asp:TextBox>  
+          <asp:RequiredFieldValidator ID="ReqLocationName" runat="server" ControlToValidate="txtLocationName" ErrorMessage="*" ForeColor="Red" ValidationGroup="GroupFooterInsert">
+         </asp:RequiredFieldValidator>
+    </FooterTemplate>
+</asp:TemplateField>
+
+<asp:TemplateField>
+    <ItemTemplate>
+        <asp:LinkButton ID="lnkRemove" runat="server" CommandArgument = '<%# Eval("Id")%>' 
+         OnClientClick = "return confirm('Would you like to inactivate this record?')" 
+        Text = "Inactive" OnClick = "InactivateLocation" CausesValidation="false"></asp:LinkButton>
+    </ItemTemplate>
+
+   
+    <FooterTemplate>
+      
+            <asp:Button ID="btnAdd" runat="server" Text="Add Location" OnClick = "AddNewLocation" ValidationGroup="GroupFooterInsert"   />
+           
+    </FooterTemplate>
+    
+
+</asp:TemplateField>
+
+      <asp:CommandField  ShowEditButton="True" />
+
+</Columns>
+<AlternatingRowStyle BackColor="white"  />
+</asp:GridView>
+            </ContentTemplate> 
+    <Triggers>
+        <asp:AsyncPostBackTrigger ControlID = "GridView1" /> 
+    </Triggers> 
+    </asp:UpdatePanel> 
+
+
+</div>
+
+</asp:Content>
---
+++ RingDownCentralConsole/Administration/Locations.aspx.cs
@@ -0,0 +1,177 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Data;
+using System.Configuration;
+
+
+namespace RingDownCentralConsole
+{
+    public partial class Locations : System.Web.UI.Page
+    {
+      
+        string constr = ConfigurationManager.ConnectionStrings["csConsole"].ToString();
+        
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                //BindData();
+                BindData();                
+            }            
+        }
+
+
+        private void BindData()
+        {
+            string strQuery = "SELECT * from Locations Where IsActive=1";
+            //  SqlCommand cmd = new SqlCommand(strQuery);
+            var cmd = new SqlCommand(strQuery);
+            GridView1.DataSource = GetData(cmd);
+            GridView1.DataBind();           
+        }
+
+
+        private DataTable GetData(SqlCommand cmd)
+        {
+            DataTable dt = new DataTable();
+            SqlConnection con = new SqlConnection(constr);
+            SqlDataAdapter sda = new SqlDataAdapter();
+            cmd.CommandType = CommandType.Text;
+            cmd.Connection = con;
+            con.Open();
+            sda.SelectCommand = cmd;
+            sda.Fill(dt);
+            return dt;
+        }
+
+               
+        protected void InactivateLocation(object sender, EventArgs e)
+        {         
+            
+            using (SqlConnection con = new SqlConnection(constr))
+            {
+                LinkButton lnkRemove = (LinkButton)sender;
+                try
+                {
+                    SqlCommand cmd = new SqlCommand();
+                    cmd.CommandType = CommandType.Text;
+                    cmd.CommandText = "Update Locations set IsActive=@IsActive Where Id=@Id;" +
+                     "Select * from Locations Where IsActive=1";
+                    cmd.Parameters.Add("@Id", SqlDbType.VarChar).Value = lnkRemove.CommandArgument;
+                    cmd.Parameters.Add("@IsActive", SqlDbType.Bit).Value = 0;
+
+                    GridView1.EditIndex = -1;
+                    GridView1.DataSource = GetData(cmd);
+                    GridView1.DataBind();
+                    BindData();
+                }
+                catch (Exception ex)
+                {
+                    /*Handle error*/
+                    Msg.Text = "Connection Error in InactivateLocation module" + ex;
+                }
+
+            }
+            
+           
+        }
+
+        protected void OnPaging(object sender, GridViewPageEventArgs e)
+        {
+            BindData();
+            GridView1.PageIndex = e.NewPageIndex;
+            GridView1.DataBind();
+        }
+
+
+        protected void EditLocation(object sender, GridViewEditEventArgs e)
+        {
+            GridView1.EditIndex = e.NewEditIndex;
+            BindData();
+        }
+
+        protected void CancelEdit(object sender, GridViewCancelEditEventArgs e)
+        {
+            GridView1.EditIndex = -1;
+            BindData();
+        }
+
+        protected void UpdateLocation(object sender, GridViewUpdateEventArgs e)
+        {
+            using (SqlConnection con = new SqlConnection(constr))
+            {
+                string Id = ((Label)GridView1.Rows[e.RowIndex].FindControl("lblLocationId")).Text;
+                string LoationName = ((TextBox)GridView1.Rows[e.RowIndex].FindControl("txtLocationName")).Text;
+                string Code = ((TextBox)GridView1.Rows[e.RowIndex].FindControl("Code")).Text;
+
+                try
+                {
+                    SqlCommand cmd = new SqlCommand();
+                    cmd.CommandType = CommandType.Text;
+                    cmd.CommandText = "Update Locations set Code=@Code, " +
+                     "LocationName=@LocationName where Id=@Id;Select * From Locations WHERE IsActive=1";
+
+                    cmd.Parameters.Add("@Id", SqlDbType.Int).Value = Id;
+                    cmd.Parameters.Add("@LocationName", SqlDbType.VarChar).Value = LocationName;
+                    cmd.Parameters.Add("@Code", SqlDbType.VarChar).Value = Code;
+
+                    GridView1.EditIndex = -1;
+                    GridView1.DataSource = GetData(cmd);
+                    GridView1.DataBind();
+                }
+                catch (Exception ex)
+                {
+                    /*Handle error*/
+                    Msg.Text = "Connection Error in UpdateStation module" + ex;
+                }
+
+            }
+            
+        }
+              
+
+        protected void AddNewLocation(object sender, EventArgs e)
+        {
+
+            using (SqlConnection con = new SqlConnection(constr))
+            {
+                string Code = ((TextBox)GridView1.FooterRow.FindControl("txtLocationCode")).Text;
+                string LocationName = ((TextBox)GridView1.FooterRow.FindControl("txtLocationName")).Text;
+
+                try
+                {
+                    SqlCommand cmd = new SqlCommand();
+                    cmd.CommandType = CommandType.Text;
+                    //wow made the mistake of leaving out the station id field in select statement (drove me crazzzzy!)
+                    cmd.CommandText = "Insert into tblStations (Code, LocationName, IsActive) " +
+                    "values (@Code, @LocationName, @IsActive);" +
+                    "Select * From Locations WHERE IsActive=1";
+                    cmd.Parameters.Add("@Code", SqlDbType.VarChar).Value = Code;
+                    cmd.Parameters.Add("@LocationName", SqlDbType.VarChar).Value = LocationName;
+                    cmd.Parameters.Add("@IsActive", SqlDbType.Bit).Value = 1;
+
+                    GridView1.DataSource = GetData(cmd);
+                    GridView1.DataBind();
+                    BindData();
+                }
+                catch (Exception ex)
+                {
+                    /*Handle error*/
+                    Msg.Text = "Connection Error in AddNewLocation module" + ex;
+                }
+
+            }
+
+
+        }
+
+
+
+       
+    }
+}
