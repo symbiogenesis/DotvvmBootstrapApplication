1120e6dbf3c816627715d9be32195f26e2bf2cd8
Edward Miller <edward.miller@catconsult.com>
2018-01-16 10:40:19 -0600

    use Identity Framework for login

--- RingDownCentralConsole/Login.aspx.cs
+++ RingDownCentralConsole/Login.aspx.cs
@@ -1,10 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Web;
-using System.Web.Security;
-using System.Web.UI;
-using System.Web.UI.WebControls;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.Owin.Security;
 
 namespace RingDownCentralConsole
 {
@@ -12,18 +10,51 @@ namespace RingDownCentralConsole
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-           
-        }
-
-        protected void Login1_LoggedIn(object sender, EventArgs e)
-        {
+            if (!IsPostBack)
             {
-                if (Roles.IsUserInRole(Login1.UserName, "Administrator"))
-                    Response.Redirect("~/Administration/Dashboard.aspx");
-                else if (Roles.IsUserInRole(Login1.UserName, "User"))
-                    Response.Redirect("~/RDCC/MainConsole.aspx");
+                if (User.Identity.IsAuthenticated)
+                {
+                    StatusText.Text = string.Format("Hello {0}!!", User.Identity.GetUserName());
+                    LoginStatus.Visible = true;
+                    LogoutButton.Visible = true;
+                }
+                else
+                {
+                    LoginForm.Visible = true;
+                }
             }
         }
 
+        protected void SignIn(object sender, EventArgs e)
+        {
+            var userStore = new UserStore<IdentityUser>();
+            var userManager = new UserManager<IdentityUser>(userStore);
+            var user = userManager.Find(UserName.Text, Password.Text);
+
+            if (user != null)
+            {
+                var authenticationManager = HttpContext.Current.GetOwinContext().Authentication;
+                var userIdentity = userManager.CreateIdentity(user, DefaultAuthenticationTypes.ApplicationCookie);
+
+                authenticationManager.SignIn(new AuthenticationProperties() { IsPersistent = false }, userIdentity);
+
+                if (userManager.IsInRole(userIdentity.GetUserId(), "Administrator" ))
+                    Response.Redirect("~/Administration/Dashboard.aspx");
+                else
+                    Response.Redirect("~/RDCC/MainConsole.aspx");
+            }
+            else
+            {
+                StatusText.Text = "Invalid username or password.";
+                LoginStatus.Visible = true;
+            }
+        }
+
+        protected void SignOut(object sender, EventArgs e)
+        {
+            var authenticationManager = HttpContext.Current.GetOwinContext().Authentication;
+            authenticationManager.SignOut();
+            Response.Redirect("~/Login.aspx");
+        }
     }
 }
