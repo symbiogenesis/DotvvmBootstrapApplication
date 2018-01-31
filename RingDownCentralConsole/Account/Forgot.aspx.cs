834fd2692215564a922f8efc5e26d19f461fc640
Edward Miller <edward.miller@catconsult.com>
2018-01-31 09:53:29 -0600

    find by email

--- RingDownCentralConsole/Account/Forgot.aspx.cs
+++ RingDownCentralConsole/Account/Forgot.aspx.cs
@@ -20,7 +20,7 @@ namespace RingDownCentralConsole.Account
             {
                 // Validate the user's email address
                 var manager = Context.GetOwinContext().GetUserManager<ApplicationUserManager>();
-                ApplicationUser user = manager.FindByName(Email.Text);
+                ApplicationUser user = manager.FindByEmail(Email.Text);
                 if (user == null || !manager.IsEmailConfirmed(user.Id))
                 {
                     FailureText.Text = "The user either does not exist or is not confirmed.";
