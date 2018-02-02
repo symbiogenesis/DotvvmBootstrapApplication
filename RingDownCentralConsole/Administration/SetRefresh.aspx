284c85c81d503cb4033fe50b0e12e31d72e000f0
Reginald Dean <reginald.dean@cityofchicago.org>
2018-02-02 17:07:56 -0600

    SetRefresh.aspx allows admins to set website refresh interval.

---
+++ RingDownCentralConsole/Administration/SetRefresh.aspx
@@ -0,0 +1,40 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="SetRefresh.aspx.cs" Inherits="RingDownCentralConsole.Administration.SetRefresh" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
+
+    <link rel="stylesheet" type="text/css" href="rdccCSS.css" /> 
+    <div  class="center-div">
+        Set Refresh Interval</div>
+    <div>
+        <p><asp:Label id="Msg" runat="server" ForeColor="maroon" /><br /></p>
+    </div>
+       <br />
+
+    <p>
+
+        <asp:Label ID="Label1" runat="server" Text="Use the drop box to select the number of seconds (1 second = 1000 milliseconds)."></asp:Label>
+    </p>
+    <br />
+    <p>
+
+    <asp:DropDownList ID="ddlIntervalSeconds" runat="server">
+        <asp:ListItem Selected="True">1000</asp:ListItem>
+        <asp:ListItem>2000</asp:ListItem>
+        <asp:ListItem>3000</asp:ListItem>
+        <asp:ListItem>4000</asp:ListItem>
+          <asp:ListItem>5000</asp:ListItem>
+        <asp:ListItem>6000</asp:ListItem>
+         <asp:ListItem>7000</asp:ListItem>
+          <asp:ListItem>8000</asp:ListItem>
+          <asp:ListItem>9000</asp:ListItem>
+          <asp:ListItem>10000</asp:ListItem>     
+    </asp:DropDownList>
+
+
+    </p>
+
+    
+    <br />
+    <asp:TextBox ID="txtRefreshVal" runat="server" BackColor="#EFF3FB" ReadOnly="true"></asp:TextBox>
+    <br /><br />
+    <asp:Button ID="Button1" runat="server" Text="Update"  OnClick="SetInterval_Click"/>
+</asp:Content>
---
+++ RingDownCentralConsole/Administration/SetRefresh.aspx.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using Newtonsoft.Json;
+
+namespace RingDownCentralConsole.Administration
+{
+
+   
+
+    public partial class SetRefresh : System.Web.UI.Page
+    {
+        private readonly string _path = "~/Interval/RefreshVal.json";
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if ((User.Identity.IsAuthenticated) && (User.IsInRole("Administrator")))
+            {
+                BindData();
+             
+            }
+            else
+            {
+                //Send user back to main console page, because user is not an "Administrator" role
+                Response.Redirect("~/Account/Login.aspx");
+            }
+        }
+
+
+        private void BindData()
+        {
+
+           
+            try
+            {
+                using (var sr = new StreamReader(Server.MapPath(_path)))
+                {
+                    txtRefreshVal.Text = sr.ReadLine();
+                }
+            }
+            catch (Exception ex)
+            {
+                Msg.Text = "Page Load Error:" + ex.ToString();
+            }
+
+
+        }
+
+        protected void SetInterval_Click(object sender, EventArgs e)
+        {
+            //var sb = new StringBuilder();
+            //var sw = new StringWriter(sb);
+
+            var seconds = ddlIntervalSeconds.SelectedValue.ToString();          
+
+            //Update .json file with value, replace each value
+            using (var _val = new StreamWriter(Server.MapPath(_path), false))
+            {
+                try
+                {
+                    //_testData.Write(seconds); // Write the file.
+                    _val.WriteLine(seconds);
+                    _val.Flush();
+                    _val.Close();
+                    _val.Dispose();
+
+                    Msg.Text = "The Ring Down Central Console refresh interval has been set to " + seconds + " seconds.";
+
+                    BindData();
+                  
+                }
+                catch (IOException ex)
+                {
+                    Msg.Text = "IOException:\r\n\r\n" + ex.Message;                   
+                }
+                catch (Exception ex)
+                {                   
+                    Msg.Text = "Exception:\r\n\r\n" + ex.Message;
+                }
+                finally
+                {
+                    if (_val != null)
+                    {
+                      //  _val.Flush();
+                      ////  _val.Close();
+                      //  _val.Dispose();
+                       
+                    }
+                }
+
+
+
+
+
+
+
+            }
+        }
+    }
+}
