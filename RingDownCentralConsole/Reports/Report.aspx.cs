64c8753476527327a34f08edfaf6b6641ee7b761
Reginald Dean <reginald.dean@cityofchicago.org>
2018-02-15 11:27:21 -0600

    Use GetData() instead of BindData() in order of the export feature to work properly.

--- RingDownCentralConsole/Reports/Report.aspx.cs
+++ RingDownCentralConsole/Reports/Report.aspx.cs
@@ -100,7 +100,7 @@ namespace RingDownCentralConsole.Reports
 
                 //To Export all pages
                 GridView1.AllowPaging = false;
-                this.BindData();
+                this.GetData();
 
                 GridView1.HeaderRow.BackColor = Color.White;
                 foreach (TableCell cell in GridView1.HeaderRow.Cells)
