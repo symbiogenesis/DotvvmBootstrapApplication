1e2d3089d7d2d1df7a455a4d69a8192893a784aa
Edward Miller <edward.miller@catconsult.com>
2018-02-23 09:45:40 -0600

    Add WebAPI example

---
+++ RingDownConsole.WebAPI/Controllers/ExampleRecordController.cs
@@ -0,0 +1,11 @@
+﻿using DotvvmBootstrapApplication.Models;
+
+namespace DotvvmBootstrapApplication.WebAPI.Controllers
+{
+    public class ExampleRecordController : BaseController<ExampleRecord>
+    {
+        public ExampleRecordController(BootstrapDbContext context) : base(context)
+        {
+        }
+    }
+}
